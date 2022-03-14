eb0146bedb33c42b26267fcaa60231c71dd6973c
osefatul <sefaullahomar2@gmail.com>
2022-03-14 00:35:12 -0700

    assigned alan SDK key

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # React AI Speech Recognition Web Application
+
+We are using ALAN AI for our speech recognition software. You can visit to the website by clicking [here](https://alan.app/).
+
+Signup and then start an empty project. Click on the integration and copy SDK key. Go to APP.js file and assign the key to alanKey.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import wordsToNumbers from "words-to-numbers";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
+const alanKey =
+  "574b14d84880a9d5b2a102dc6860d98c2e956eca572e1d8b807a3e2338fdd0dc/stage";
 function App() {
-  return <div className="flex items-center  justify-center text-4xl">Hi</div>;
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command})=>{
+        if(command)
+      }
+    });
+  }, []);
+  return (
+    <div className="bg-whitesmoke flex items-center justify-center text-4xl font-bold py-8">
+      <h1>Hi</h1>
+    </div>
+  );
 }
 
 export default App;
