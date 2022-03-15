0639c4747ab8e5b4361195365cb152f33d24c570
osefatul <sefaullahomar2@gmail.com>
2022-03-15 01:36:53 -0700

    working on newscard design

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -4,27 +4,33 @@ function NewsCard({
   article: { description, publishedAt, source, title, url, urlToImage },
 }) {
   return (
-    <div className="group cursor-pointer border rounded-lg overflow-hidden bg-slate-50">
-      <div className="py-">
-        <div></div>
-        <img
-          className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
-          src={
-            urlToImage ||
-            "https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png"
-          }
-        />
-      </div>
-      <div className="p-2">
-        <p className="text-sm">{source.name}</p>
-        <h3 className="text-lg font-bold">{title}</h3>
-        <p>{description}</p>
-      </div>
-      <div className="flex justify-between p-2 items-center ">
-        <p className="font-extralight text-sm">
-          {new Date(publishedAt).toDateString()}
-        </p>
-      </div>
+    <div>
+      <a href={url}>
+        <div className="group cursor-pointer border rounded-lg overflow-hidden bg-slate-50 h-full">
+          <div className="">
+            <div></div>
+            <img
+              className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+              src={
+                urlToImage ||
+                "https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png"
+              }
+            />
+          </div>
+
+          <div className="py-3 px-2">
+            <div className="flex justify-between pb-2 items-center ">
+              <p className="text-sm font-medium">{source.name}</p>
+              <p className="font-extralight text-sm">
+                {new Date(publishedAt).toDateString()}
+              </p>
+            </div>
+
+            <h3 className="text-lg font-bold">{title}</h3>
+            <p>{description}</p>
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
