c8f95b5085304d5e69ff6ba4f631f15812d7824a
osefatul <sefaullahomar2@gmail.com>
2022-03-14 23:44:44 -0700

    working on newscard

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -3,7 +3,11 @@ import React from "react";
 function NewsCard({
   article: { description, publishedAt, source, title, url, urlToImage, author },
 }) {
-  return <div></div>;
+  return (
+    <div>
+      <div href={url}></div>
+    </div>
+  );
 }
 
 export default NewsCard;
