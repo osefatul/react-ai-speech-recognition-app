7951c6c0f67ad8e0619f1c9f5e7abcd4997be376
osefatul <sefaullahomar2@gmail.com>
2022-03-14 23:15:01 -0700

    working on newscard

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -3,9 +3,11 @@ import NewsCard from "../NewsCard/NewsCard";
 
 function NewsCards({ articles }) {
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 lg:gap-24 p-2 md:p-6 ">
       {articles.map((article, i) => (
-        <NewsCard />
+        <div>
+          <NewsCard article={article} />
+        </div>
       ))}
     </div>
   );
