f5b655f323de89094e52ee5df2908cde60fe3eaa
osefatul <sefaullahomar2@gmail.com>
2022-03-15 20:15:20 -0700

    InfoCard has been update

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,9 +1,64 @@
 import React from "react";
 import NewsCard from "../NewsCard/NewsCard";
 
+const infoCards = [
+  {
+    color: "bg-[#00838f]",
+    title: "Latest News",
+    text: "Give me the latest news",
+  },
+  {
+    color: "bg-[#1565c0]",
+    title: "News by Categories",
+    info: "Business, Entertainment, General, Health, Science, Sports, Technology",
+    text: "Give me the latest Technology news",
+  },
+  {
+    color: "bg-[#4527a0]",
+    title: "News by Terms",
+    info: "Bitcoin, PlayStation 5, Smartphones, Donald Trump...",
+    text: "What's up with PlayStation 5",
+  },
+  {
+    color: "bg-[#283593]",
+    title: "News by Sources",
+    info: "CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...",
+    text: "Give me the news from CNN",
+  },
+];
+
 function NewsCards({ articles }) {
+  // Sp before someone ask Alan we need to have components of guide
+  if (!articles.length) {
+    return (
+      <div
+        className="grid grid-cols-1 
+      sm:grid-cols-2 
+      lg:grid-cols-3 
+      xl:grid-cols-4 
+      2xl:grid-cols-6 
+      gap-2 md:gap-5 
+      lg:gap-8 p-2 
+      md:p-3 "
+      >
+        {infoCards.map((infoCard, i) => (
+          <div className={infoCard.color}> Hi </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-2 md:gap-5 lg:gap-8 p-2 md:p-3 ">
+    <div
+      className="grid grid-cols-1 
+    sm:grid-cols-2 
+    lg:grid-cols-3 
+    xl:grid-cols-4 
+    2xl:grid-cols-6 
+    gap-2 md:gap-5 
+    lg:gap-8 p-2 
+    md:p-3 "
+    >
       {articles.map((article, i) => (
         <NewsCard i={i} article={article} />
       ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // colors: {
+      //   LatesNews: "#00838f",
+      //   CategoryNews: "#1565c0",
+      //   TermsNews: "#4527a0",
+      //   SourcesNews: "#283593",
+      // },
+    },
   },
   plugins: [],
 };
